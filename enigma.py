5c70181669b2ae08e7ecde211671f782c954fd95
Welf Poser <welf.poser@gmail.com>
2025-02-26 22:00:46 +0100

    add functionality for encryption. its buggy as hell!

--- enigma.py
+++ enigma.py
@@ -1,7 +1,7 @@
 #Wehrmacht Enigma I (M3)
 
 class Enigma:
-    def __init__(self, reflector, rotor3, pos3, rotor2, pos2, rotor1, pos1, plugboard):
+    def __init__(self, reflector, rotor1, pos1, rotor2, pos2, rotor3, pos3, plugboard):
         self.reflector = reflector
         self.rotor1 = rotor1
         self.rotor2 = rotor2
@@ -12,51 +12,123 @@ class Enigma:
         self.pos2 = pos2
         self.pos3 = pos3
 
-        def prep_message(msg):
+        """
+        initial_settings = {
+            "plugboard" : plugboard.copy(),
+            "pos1" : pos1.copy(),
+            "rotor1" : rotor1.copy(),
+            "pos2" : pos2.copy(),
+            "rotor2" : rotor2.copy(),
+            "pos3" : pos3.copy(),
+            "rotor3" : rotor3.copy(),
+            "reflector" : reflector.copy(),
+        }
+        """
 
-            msg = msg.upper()
-            prep_msg = ""
-            for char in msg:
-                if ord(char) == 32 or ord(char) >= 65 or ord(char) <= 90:
-                    prep_msg += char
-                
-            return prep_msg
+    def prep_message(self, msg):
+
+        msg = msg.upper()
+        prep_msg = ""
+        for char in msg:
+            if ord(char) == 32 or ord(char) >= 65 or char == " " or char == "\n":
+                prep_msg += char
+            
+        return prep_msg
+    
+    def encrypt(self, msg):
+
+        msg = self.prep_message(msg)
+        msg_encrypt = ""
+
+        for char in msg:
+
+            if char == " " or char == "\n":
+                msg_encrypt += char
+                continue
+
+            print("Keyboard Input: " + char)
+
+            self.pos3 = (self.pos3 % 26) + 1
+
+            if self.pos3 in self.rotor3.get_notch():
+                self.pos2 = (self.pos2 % 26) + 1
+
+                # Double Step Mechanismus für den mittleren Rotor
+                if self.pos2 in self.rotor2.get_notch():
+                    self.pos2 = (self.pos2 % 26) + 1
+                    self.pos1 = (self.pos1 % 26) + 1
+
+            print("Rotors Position: " + chr(self.pos1 + 64) + ", " +
+                chr(self.pos2 + 64) + ", " + chr(self.pos3 + 64))
+
+            # Verschlüsselungsschritt:
+            char = self.plugboard.swap(char)
+            print("Plugboard Encryption: " + char)
+
+            char = self.rotor3.swap(chr(((ord(char) - ord("A") + self.pos3) % 26) + ord("A")))
+            print("Wheel 3 Encryption: " + char)
+
+            char = self.rotor2.swap(chr(((ord(char) - ord("A") + self.pos2) % 26) + ord("A")))
+            print("Wheel 2 Encryption: " + char)
+
+            char = self.rotor1.swap(chr(((ord(char) - ord("A") + self.pos1) % 26) + ord("A")))
+            print("Wheel 1 Encryption: " + char)
+
+            char = self.reflector.swap(char)  # Reflektor bleibt unverändert
+            print("Reflector Encryption: " + char)
+
+            # Rückweg: Hier wird die Positionsverschiebung rückgängig gemacht,
+            # aber auch hier keine erneute Berücksichtigung der ring_setting, da diese schon eingearbeitet wurde.
+            char = chr(((ord(char) - ord("A") - self.pos1) % 26) + ord("A"))
+            char = self.rotor1.reverse_swap(char)
+            print("Wheel 1 Reverse Encryption: " + char)
+
+            char = chr(((ord(char) - ord("A") - self.pos2) % 26) + ord("A"))
+            char = self.rotor2.reverse_swap(char)
+            print("Wheel 2 Reverse Encryption: " + char)
+
+            char = chr(((ord(char) - ord("A") - self.pos3) % 26) + ord("A"))
+            char = self.rotor3.reverse_swap(char)
+            print("Wheel 3 Reverse Encryption: " + char)
+
+            char = self.plugboard.swap(char)
+            print("Plugboard Encryption: " + char)
+            print("-----------------------------\n")
+
+            msg_encrypt += char
+
+        return msg_encrypt
+
+
+
+
+
+    def decrypt(self, msg):
+        return self.encrypt(msg)
+
+    def change_pos(self, rotor, pos):
         
-        def encrypt(self, msg):
+        if pos < 1 or pos > 26:
+            raise ValueError("No valid position: " + str(pos))
+        
+        if rotor < 1 or rotor > 3:
+            raise ValueError("No valid rotor-position: " + str(rotor))
 
-            msg = prep_message(msg)
+        match rotor:
+            case 1:
+                self.pos1 = pos
+                print("Rotor 1 changed to position " + str(pos))
+            case 2:
+                self.pos2 = pos
+                print("Rotor 2 changed to position " + str(pos))
+            case 3:
+                self.pos3 = pos
+                print("Rotor 3 changed to position " + str(pos))
 
-
-            for char in msg:
-                plugboard.swap(char)
-
-                #Hier weitermachen!
-
-
-
-
-        def decrypt(self, msg):
-            return encrypt(msg)
-
-        def change_pos(self, rotor, pos):
-            
-            if pos < 1 or pos > 26:
-                raise ValueError("No valid position: " + str(pos))
-            
-            if rotor < 1 or rotor > 3:
-                raise ValueError("No valid rotor-position: " + str(rotor))
-
-            match rotor:
-                case 1:
-                    self.pos1 = pos
-                    print("Rotor 1 changed to position " + str(pos))
-                case 2:
-                    self.pos2 = pos
-                    print("Rotor 2 changed to position " + str(pos))
-                case 3:
-                    self.pos3 = pos
-                    print("Rotor 3 changed to position " + str(pos))
-
+    """
+    def get_init_settings(self):
+        return initial_settings
+    """
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from rotor import Rotor
 from reflector import Reflector
 from plugboard import Plugboard
+from enigma import Enigma
 
+plugboard = Plugboard({
+                        "B":"Q", 
+                        "C":"R",
+                        "D":"I",
+                        "E":"J",
+                        "K":"W",
+                        "M":"T",
+                        "O":"S",
+                        "P":"X",
+                        "U":"Z",
+                        "G":"H"
+                    })
 
+rotor1 = Rotor(1, 1)
+rotor2 = Rotor(2, 1)
+rotor3 = Rotor(3, 1)
+
+reflector = Reflector("B")
+
+enigma = Enigma(reflector, rotor1, 1, rotor2, 1, rotor3, 1, plugboard,)
+
+orig_msg = ("The crucial point is: by changing ourselves, we change the world. As we become more loving on the inside, healing occurs on the outside. Much like the rising of the sea level lifts all ships, so the radiance of unconditional love within a human heart lifts all of life!")
+
+test = "AAAA BBBB CCCC DDDD"
+
+encrypt_msg = enigma.encrypt(test)
+
+print(encrypt_msg)
