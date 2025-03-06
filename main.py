c9aebb289d8b1a4afe904b292d6137b4c449a965
Welf Poser <welf.poser@gmail.com>
2025-03-06 21:51:42 +0100

    deleted old code

--- main.py
+++ main.py
@@ -33,29 +33,3 @@ for i in range(8):
     for key, value in dict.items():
         list.append(ord(key)-64)
     print(list)
-
-"""
-reflector = Reflector("B")
-
-enigma = Enigma(reflector, rotor3, 1, rotor2, 1, rotor1, 1, plugboard,)
-
-orig_msg = ("The crucial point is: by changing ourselves, we change the world. As we become more loving on the inside, healing occurs on the outside. Much like the rising of the sea level lifts all ships, so the radiance of unconditional love within a human heart lifts all of life!")
-
-test = "AAA"
-
-print(enigma.rotor1.rotor)
-print(enigma.rotor1.inverse_rotor)
-encrypt_msg = enigma.encrypt(test)
-print(encrypt_msg)
-
-enigma = Enigma(reflector, rotor3, 1, rotor2, 1, rotor1, 1, plugboard,)
-encrypt_msg = enigma.encrypt(encrypt_msg)
-print(encrypt_msg)
-
-
-    A -> D
-    AA -> NZ
-    AAA -> AYA
-"""
-
-rotor
