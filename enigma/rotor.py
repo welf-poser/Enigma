a9f98f79f9f98052f782b24fee1e7f32d2c09c89
Welf Poser <welf.poser@gmail.com>
2025-05-04 12:28:58 +0200

    rotor class created with swap function

---
+++ enigma/rotor.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Rotor: 
+    def __init__(self, rotor_type, pos):
+        self.rotor_type = rotor_type
+        self.pos = pos
+
+        self.rotors = {
+                1 : ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'],
+                2 : ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'],
+                3 : ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'],
+                4 : ['E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W', 'B'],
+                5 : ['V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K'],
+                6 : ['J', 'P', 'G', 'V', 'O', 'U', 'M', 'F', 'Y', 'Q', 'B', 'E', 'N', 'H', 'Z', 'R', 'D', 'K', 'A', 'S', 'X', 'L', 'I', 'C', 'T', 'W'],
+                7 : ['N', 'Z', 'J', 'H', 'G', 'R', 'C', 'X', 'M', 'Y', 'S', 'W', 'B', 'O', 'U', 'F', 'A', 'I', 'V', 'L', 'P', 'E', 'K', 'Q', 'D', 'T'],
+                8 : ['F', 'K', 'Q', 'H', 'T', 'L', 'X', 'O', 'C', 'B', 'J', 'S', 'P', 'D', 'Z', 'R', 'A', 'M', 'E', 'W', 'N', 'I', 'U', 'Y', 'G', 'V'],
+             }
+        
+        self.rotor = deque(self.rotors[rotor_type])
+        self.rotor.rotate(pos -1)
+
+    def swap(self, letter):
+
+        index = ord(str.upper(letter))- 65 # -65 weil Index ab 0 zählen muss
+        return self.rotor[index]
---
+++ main.py
@@ -0,0 +1,7 @@
+from enigma.rotor import Rotor
+
+r1 = Rotor(1, 2)
+
+swap = r1.swap('a')
+
+print(swap)
